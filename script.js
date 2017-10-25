0b33d38725f46ab93ac6916825184d1e8c422345
cafebabel-webmaster <webmaster@cafebabel.com>
2017-10-25 16:44:42 +0200

    fix review spaces + external link new tab

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
+/* open menu */
 var menuButton = document.querySelector('button.menu_button');
 menuButton.addEventListener('click', () => {
     menuButton.classList.toggle('active');
 });
+
+/* external link in new tab https://stackoverflow.com/questions/12987963/open-external-links-in-a-new-tab-without-jquery */
+function externalLinks() {
+    for(var c = document.getElementsByTagName("a"), a = 0; a < c.length; a++) {
+        var b = c[a];
+        b.getAttribute("href") && b.hostname !== location.hostname && (b.target = "_blank")
+    }
+}
+externalLinks();
