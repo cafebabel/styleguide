7531f1eb403e598de6f6911f98745777ba3382bb
cafebabel-webmaster <webmaster@cafebabel.com>
2017-10-26 16:29:15 +0200

    useless line + class button.menu-button update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /* open menu */
-const menuButton = document.querySelector('button.menu_button');
+const menuButton = document.querySelector('button.menu-button');
 menuButton.addEventListener('click', () => {
     menuButton.classList.toggle('active');
 });
